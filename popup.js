724b6008d2f2b56e54e865a01467e7a4786ecd53
pctrabajo <javierpadin661@gmail.com>
2024-07-28 10:31:53 -0500

    cambios

--- popup.js
+++ popup.js
@@ -22,67 +22,67 @@ document.getElementById("boleta").addEventListener("click", function() {
   });
 });
 
-
-
-let selectedProduct = ''; 
-let selectedProductIGV = false;  
-
-document.getElementById('searchInput').addEventListener('input', function() {
-  const searchValue = this.value.toUpperCase();
-  // Simula una llamada AJAX para obtener la lista de productos desde productos.json
-  fetch('productos.json')
-    .then(response => response.json())
-    .then(data => {
-      const productList = data.productos;
-      // Filtra la lista de productos basándose en la búsqueda
-      const filteredList = productList.filter(product => product.nombre.toUpperCase().includes(searchValue));
-      // Muestra los resultados en forma de lista
-      displayResultList(filteredList);
-    })
-    .catch(error => console.error('Error al obtener los productos:', error));
-});
-
-function displayResultList(results) {
-  const resultListElement = document.getElementById('resultList');
-  // Limpia la lista antes de agregar nuevos resultados
-  resultListElement.innerHTML = '';
-  // Agrega un elemento de lista por cada resultado
-  results.forEach(product => {
-    const listItem = document.createElement('li');
-    listItem.textContent = `${product.nombre} - IGV: ${product.igv ? 'Sí' : 'No'}`;
-    // Al hacer clic en el elemento de lista, almacena el nombre y el estado de IGV en las variables globales
-    listItem.addEventListener('click', function() {
-      selectedProduct = product.nombre;
-      selectedProductIGV = product.igv;
-      document.getElementById('searchInput').value = selectedProduct;
-    });
-    resultListElement.appendChild(listItem);
-  });
-}
-
-
-
-
-document.getElementById('cargardatos').addEventListener('click', function() {
-  var cantidad = document.getElementById('cantidad').value;
-  var precio = document.getElementById('precio').value;
-  var Producto = selectedProduct;  // Utiliza el nombre del producto seleccionado
-  // Verifica si el producto tiene IGV y ajusta el precio en consecuencia
-  if (selectedProductIGV) {
-    precio = (parseFloat(precio) / 1.18).toFixed(2);
-  }
-
-  // Enviar datos a conten.js
-  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, { action: 'enviarDatos', data: {Producto: Producto, precio: precio, cantidad: cantidad}});
-  });
-
-  
-});
-// popup.js (o donde sea que manejes el clic del botón)
-document.getElementById('prueba').addEventListener('click', function() {
+document.getElementById("imprimir").addEventListener("click", function() {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {action: "prueba"});
+    chrome.tabs.sendMessage(tabs[0].id, {action: "imprimir"});
   });
 });
+
+
+document.getElementById("copiar").addEventListener("click", function() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {action: "copiar"});
+  });
+});
+
+document.getElementById("pegar").addEventListener("click", function() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {action: "pegar"});
+  });
+});
+
+
+
+
+
+document.getElementById("editar").addEventListener("click", function() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {action: "editar"});
+  });
+});
+document.getElementById("imprimrtikrt").addEventListener("click", function() {
+console.log("ddd")
+  const contenidoDiv = document.getElementById('vistaboleta').innerHTML;
+      window.print();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// Escuchar mensajes del content script o del background script
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  if (message.action === "datosObtenidos") {
+    const datos = message.datos;
+    // Filtrar los datos para obtener solo "Total Venta"
+    const totalVenta = datos.find(dato => dato.label === "Total Venta");
+    if (totalVenta) {
+      // Mostrar el dato "Total Venta" en el popup.html
+      const vistaboleta = document.getElementById('vistaboleta');
+      vistaboleta.innerHTML = `
+      <td style="padding: 0px;text-align: center;"><img src="./logo.png" alt="Girl in a jacket" width="90" height="75"></td>
+      <p>${totalVenta.label}: ${totalVenta.valor}</p>
+      <p>${totalVenta.label}: ${totalVenta.valor}</p>
+      `;
+    }
+  }
+});
+
+{/* <p>${totalVenta.label}: ${totalVenta.valor}</p> */}
